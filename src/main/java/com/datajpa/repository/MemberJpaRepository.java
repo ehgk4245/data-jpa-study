2a0eebdd282cedb9d9c019c48c15275cb73f9a6a
ehgk4245 <ehgkrk14@naver.com>
2025-09-03 16:12:06 +0900

    init

---
+++ src/main/java/com/datajpa/repository/MemberJpaRepository.java
@@ -0,0 +1,22 @@
+package com.datajpa.repository;
+
+import com.datajpa.entity.Member;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberJpaRepository {
+
+    private final EntityManager em;
+
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
---
+++ src/test/java/com/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.datajpa.repository;
+
+import com.datajpa.entity.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired MemberJpaRepository memberJpaRepository;
+
+    @Test
+    @DisplayName("create member")
+    void t1() {
+        Member member = new Member("MemberA");
+        Member savedMember = memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+}
