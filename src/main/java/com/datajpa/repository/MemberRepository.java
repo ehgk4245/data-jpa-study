42d4daaea044ac37f96ab2bd7fdc0733b02c4e8f
ehgk4245 <ehgkrk14@naver.com>
2025-09-07 16:16:02 +0900

    commit

--- src/main/java/com/datajpa/repository/MemberRepository.java
+++ src/main/java/com/datajpa/repository/MemberRepository.java
@@ -4,6 +4,10 @@ import com.datajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
